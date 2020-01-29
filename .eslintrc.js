a99545a3d5f2f5d6a3127bb85fcd6e128f1581ec
yoyota <smarthorse6@gmail.com>
2020-01-30 08:21:56 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: [
+    "airbnb-base",
+    "plugin:promise/recommended",
+    "plugin:prettier/recommended",
+    "plugin:jest/recommended"
+  ],
+  env: {
+    node: true,
+    jest: true
+  },
+  plugins: ["jest", "prettier", "promise"],
+  rules: {
+    "no-unused-vars": ["error", { varsIgnorePattern: "_" }],
+    "prettier/prettier": [
+      "error",
+      {
+        semi: false
+      }
+    ]
+  }
+}
---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const compression = require("compression")
+const helmet = require("helmet")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(compression())
+app.use(helmet())
+
+app.get("/health", (_, res) => {
+  res.status(204).send()
+})
+
+app.get("/", (_, res) => {
+  res.send("home")
+})
+
+module.exports = app
