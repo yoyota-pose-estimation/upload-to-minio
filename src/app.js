c5dc3a35bb0763356a664140e8a37edc6782975b
yoyota <smarthorse6@gmail.com>
2020-02-01 17:05:17 +0900

    Add upload api

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const compression = require("compression")
 const helmet = require("helmet")
+const multer = require("multer")
 
 const app = express()
+const multerUpload = multer({ storage: multer.memoryStorage() })
+const s3Client = require("./s3Client")
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -14,8 +17,16 @@ app.get("/healthz", (_, res) => {
   res.status(204).send()
 })
 
-app.get("/", (_, res) => {
-  res.send("home")
+app.post("/:bucket/:section", multerUpload.single("file"), async (req, res) => {
+  const { bucket, section } = req.params
+  const { buffer, size, originalname } = req.file
+  await s3Client.putObject(
+    bucket,
+    `${section}/${new Date().toLocaleDateString("ko-KR")}/${originalname}`,
+    buffer,
+    size
+  )
+  res.status(204).send()
 })
 
 module.exports = app
--- src/app.test.js
+++ src/app.test.js
@@ -1,9 +1,45 @@
 const request = require("supertest")
 const app = require("./app")
+const s3Client = require("./s3Client")
 
-test("test root route", async () => {
-  const res = await request(app).get("/")
-  expect(res.status).toEqual(200)
+const testBucketName = "test-bucket"
+
+async function createTestBucket() {
+  const buckets = await s3Client.listBuckets()
+  const testBucket = buckets.find(({ name }) => name === testBucketName)
+  if (!testBucket) {
+    await s3Client.makeBucket(testBucketName)
+  }
+}
+
+describe("upload test", () => {
+  beforeAll(async () => {
+    await createTestBucket()
+  })
+  test("test file upload", async () => {
+    const testTxtContent = "test content"
+
+    const res = await request(app)
+      .post(`/${testBucketName}/section`)
+      .attach("file", Buffer.from(testTxtContent), "test.txt")
+    expect(res.status).toEqual(204)
+
+    const stream = await s3Client.getObject(
+      testBucketName,
+      `section/${new Date().toLocaleDateString("ko-KR")}/test.txt`
+    )
+
+    return new Promise((resolve, reject) => {
+      const chunks = []
+      stream.on("data", chunk => chunks.push(chunk))
+      stream.on("error", reject)
+      stream.on("end", () => {
+        const content = Buffer.concat(chunks).toString("utf8")
+        expect(content).toBe(testTxtContent)
+        resolve()
+      })
+    })
+  })
 })
 
 test("test healthz route", async () => {
