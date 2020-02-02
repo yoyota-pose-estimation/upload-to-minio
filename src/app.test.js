aac108044f648cd7533d4b0c40a0f1aa370e1ef6
yoyota <smarthorse6@gmail.com>
2020-02-02 21:19:45 +0900

    Add try catch on upload

--- src/app.test.js
+++ src/app.test.js
@@ -16,13 +16,14 @@ describe("upload test", () => {
   beforeAll(async () => {
     await createTestBucket()
   })
+
   test("test file upload", async () => {
     const testTxtContent = "test content"
 
     const res = await request(app)
       .post(`/${testBucketName}/section`)
       .attach("file", Buffer.from(testTxtContent), "test.txt")
-    expect(res.status).toEqual(204)
+    expect(res.status).toBe(204)
 
     const stream = await s3Client.getObject(
       testBucketName,
@@ -39,6 +40,14 @@ describe("upload test", () => {
         resolve()
       })
     })
+  })
+
+  test("test error handling", async () => {
+    const res = await request(app).post(`/${testBucketName}/section`)
+    expect(res.status).toBe(500)
+    expect(res.text).toBe(
+      "TypeError: Cannot destructure property `buffer` of 'undefined' or 'null'."
+    )
   })
 })
 
