56cece517e779a8f1da01127a23fb99bb9fa0da6
yoyota <smarthorse6@gmail.com>
2020-02-09 17:23:07 +0900

    Add minio mock function

--- src/s3Client.test.js
+++ src/s3Client.test.js
@@ -1,6 +1,54 @@
-const s3Client = require("./s3Client")
+/* eslint-disable global-require */
+const mockClient = jest.fn()
+jest.mock("minio", () => ({
+  Client: mockClient
+}))
 
-test("test s3 client", async () => {
-  const buckets = await s3Client.listBuckets()
-  expect(buckets).toBeInstanceOf(Array)
+describe("minio test", () => {
+  const host = "localhost"
+  const port = 9000
+  const S3_ENDPOINT = `${host}:${port}`
+  const AWS_ACCESS_KEY_ID = "accessKey"
+  const AWS_SECRET_ACCESS_KEY = "secretKey"
+  beforeEach(() => {
+    process.env = {
+      ...process.env,
+      S3_ENDPOINT,
+      AWS_ACCESS_KEY_ID,
+      AWS_SECRET_ACCESS_KEY
+    }
+    jest.resetModules()
+  })
+
+  afterEach(() => {
+    mockClient.mockClear()
+  })
+
+  test("test key", async () => {
+    require("./s3Client")
+    expect(mockClient.mock.calls[0][0].accessKey).toBe(AWS_ACCESS_KEY_ID)
+    expect(mockClient.mock.calls[0][0].secretKey).toBe(AWS_SECRET_ACCESS_KEY)
+  })
+  test("test endpoint", () => {
+    require("./s3Client")
+    expect(mockClient.mock.calls[0][0].endPoint).toBe(host)
+  })
+  test("test port", () => {
+    require("./s3Client")
+    expect(mockClient.mock.calls[0][0].port).toBe(port)
+  })
+
+  test("test use ssl == 1", () => {
+    const S3_USE_HTTPS = "1"
+    process.env = { ...process.env, S3_USE_HTTPS }
+    require("./s3Client")
+    expect(mockClient.mock.calls[0][0].useSSL).toBe(true)
+  })
+
+  test("test use ssl == 0", () => {
+    const S3_USE_HTTPS = "0"
+    process.env = { ...process.env, S3_USE_HTTPS }
+    require("./s3Client")
+    expect(mockClient.mock.calls[0][0].useSSL).toBe(false)
+  })
 })
